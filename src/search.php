1f7ee4f5aa41598234412e43b70a1b2873f2e642
mairemccrystal <mairemccrystal@gmail.com>
2020-01-10 23:33:13 +0000

    search is working but needs added to

--- src/search.php
+++ src/search.php
@@ -6,26 +6,24 @@ $dbhost = "database-1.cacihsari076.us-east-1.rds.amazonaws.com";
    $dbpassword = "daveCutting123";
 
    $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
-   
+
    $search = $_GET['q'];
    echo "<pre>";
-   $searche = explode(" ", $searche);
+   $searche = explode(" ", $search);
 
    $x = 0;
-   $construct = "";
-   foreach ($searche as $term){
+   $construct ="";
+   foreach($searche as $term){
      $x++;
-     if($x == 1){ 
-       $construct.="title LIKE '%$term%' OR description LIKE '%$term%' OR keywords LIKE '%$term%'";  
+     if ($x == 1) {
+       $construct .= "title LIKE '%$term%'";
      } else {
-       $construct.="AND title LIKE '%$term%' OR description LIKE '%$term%' OR keywords LIKE '%$term%'"; 
+       $construct .= "OR title LIKE '%$term%'";
      }
    }
-   
-   
+
    $results = $pdo->query("SELECT * FROM webscraper2.index WHERE $construct");
    print_r($results->fetchAll());
-
 
 
 
