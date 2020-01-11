b7df647041a6457fc33c376b25af1faaafef40f5
mairemccrystal <mairemccrystal@gmail.com>
2020-01-11 00:18:14 +0000

    more changes - e.g query number at top

--- src/search.php
+++ src/search.php
@@ -8,7 +8,7 @@ $dbhost = "database-1.cacihsari076.us-east-1.rds.amazonaws.com";
    $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
 
    $search = $_GET['q'];
-   echo "<pre>";
+   
    $searche = explode(" ", $search);
 
    $x = 0;
@@ -16,14 +16,24 @@ $dbhost = "database-1.cacihsari076.us-east-1.rds.amazonaws.com";
    foreach($searche as $term){
      $x++;
      if ($x == 1) {
-       $construct .= "title LIKE '%$term%'";
+       $construct .= "title LIKE '%$term%' OR description LIKE '%$term%' OR keywords LIKE '%$term%'";
      } else {
-       $construct .= "OR title LIKE '%$term%'";
+       $construct .= "AND title LIKE '%$term%' OR description LIKE '%$term%' OR keywords LIKE '%$term%";
      }
    }
 
    $results = $pdo->query("SELECT * FROM webscraper2.index WHERE $construct");
+   
+
+   if ($results->rowCount() == 0){
+     echo "0 results for your query! <hr />";
+   } else {
+     echo $results->rowCount()." results found for your query! <hr />";
+   }
+
+echo "<pre>";
    print_r($results->fetchAll());
+ 
 
 
 
